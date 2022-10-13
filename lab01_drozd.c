106ba93399dca816c402aee42115206ab25b5195
Veanty <plmulticraftpl@gmail.com>
2022-10-13 14:14:15 +0200

    Feat: add 01 lab lesson to the project

---
+++ lab01_drozd.c
@@ -0,0 +1,160 @@
+// 1. Dla kazdego zadania prosze napisac osobna funkcje main w wyznaczonym miejscu.
+// 2. Dla kazdego zadania prosze wlaczyc tylko te biblioteki, ktore sa niezbedne.
+// 3. Tylko jedno zadanie w danym momencie moze byc odkomentowane.
+// 4. Na tych zajeciach nie wolno uzywac zmiennych.
+
+
+/*===========================================================================*/
+/*                               ZADANIE 1                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//int main() {
+//	printf("Oto szesciany wybranych liczb:\n");
+//	printf("\nk\tk*k*k\n");
+//	printf("\n21\t%d", 21 * 21 * 21);
+//	printf("\n32\t%d", 32 * 32 * 32);
+//	printf("\n-53\t%d", -53 * -53 * -53);
+//	printf("\n%s", "Koniec programu.");
+//	return 0;
+//}
+
+
+
+/*===========================================================================*/
+/*                               ZADANIE 2                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//int main() {
+//	printf("Oto szesciany wybranych liczb:\n");
+//	printf("\n%3s%12s","k","k*k*k\n");
+//	printf("\n%3d%11d",21, 21 * 21 * 21);
+//	printf("\n%3d%11d",32, 32 * 32 * 32);
+//	printf("\n%2d%11d",-53, -53 * -53 * -53);
+//	printf("\n%s", "Koniec programu.");
+//	return 0;
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 3                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//int main() {
+//	printf("Oto szesciany wybranych liczb\nzapisane w systemie osemkowym:\n");
+//	printf("\n%2s%30s\n","k", "k*k*k(system osemkowy)");
+//	printf("\n%2d%20o", 2, 2 * 2 * 2);
+//	printf("\n%2d%20o", 3, 3 * 3 * 3);
+//	printf("\n%2d%20o\n", 5, 5 * 5 * 5);
+//	printf("\n%s", "Koniec programu.");
+//	return 0;
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 4                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//#include <math.h>
+//int main() {
+//	printf("Oto pierwiastki kwadratowe wybranych liczb:\n");
+//	printf("\n%3s%11s","k","sqrt(k)\n");
+//	printf("\n%3d%10lf", 21, sqrt(21.0));
+//	printf("\n%3d%10lf", 32, sqrt(32.0));
+//	printf("\n%3d%10lf\n", 53, sqrt(53.0));
+//	printf("\n%s", "Koniec programu.");
+//	return 0;
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 5                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//#include <math.h>
+//int main() {
+//	printf("Oto pierwiastki kwadratowe wybranych liczb:\n");
+//	printf("\n%3s%13s", "k", "sqrt(k)\n");
+//	printf("\n%3d%25.18lf", 21, sqrt(21.0));
+//	printf("\n%3d%25.18lf", 32, sqrt(32.0));
+//	printf("\n%3d%25.18f\n",53, sqrt(53.0));
+//	printf("\n%s", "Koniec programu.");
+//	return 0;
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 6                                   */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//#include <math.h>
+//int main() {
+	//printf("Oto pierwiastki kwadratowe wybranych liczb:\n");
+	//printf("\n%6s%13s", "k", "sqrt(k)\n");
+	//printf("\n%6d%14.6le", 21, sqrt(21.0));
+	//printf("\n%6d%14.6le", 132, sqrt(132.0));
+	//printf("\n%6d%14.6le\n", 11153, sqrt(11153.0));
+	//printf("\n%s", "Koniec programu.");
+	//return 0;
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 7                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//#include <math.h>
+//int main() {
+//	printf("Oto pierwiastki kwadratowe wybranych liczb:\n");
+//	printf("\n%14s%13s", "k", "sqrt(k)\n");
+//	printf("\n%14.9lf%18.8lg",0.000000001, sqrt(0.000000001));
+//	printf("\n%14.8lf%11.8lg",0.00000001, sqrt(0.00000001));
+//	printf("\n%14lld%14.8lg",9876543210123, sqrt(9876543210123.0));
+//	printf("\n%14lld%14.8lg\n",987654321012, sqrt(987654321012.0));
+//	printf("\n%s", "Koniec programu.");
+//	return 0;
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 8                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//int main(){
+//	printf("%10d\n",111);		//Wypisuje wartoœæ
+//	printf("%+10d\n",111);		//Wypisuje "+" lub "-" w zale¿noœæi od wartoœæi podanej liczby
+//	printf("%-10d\n",111);		//Wyjustowanie do lewej
+//	printf("%010d\n",111);		//Zapelnienie spacji zerami
+//	printf("%10d\n",-111);		//Wypisuje wartoœæ
+//	printf("%+10d\n",-111);		//Wypisuje "+" lub "-" w zale¿noœæi od wartoœæi podanej liczby
+//	printf("%-10d\n",-111);		//Wyjustowanie do lewej
+//	printf("%010d\n",-111);		//Zapelnienie spacji zerami
+//	return 0;
+//}
+
+
+
+/*===========================================================================*/
+/*                               ZADANIE 9                                   */
+/*===========================================================================*/
+
+//#include <stdio.h>
+//int main() {
+//	printf("%d\n",-111);		//Flaga " " wypisuje spacje przed argumentem przy pozytywnych wartoœciach
+//	printf("%d\n",0);			
+//	printf("%d\n",111);
+//	printf("% d\n", -111);
+//	printf("% d\n", 0);
+//	printf("% d\n", 111);
+//	return 0;
+//}
+
+
