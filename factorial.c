a44a3bd3ccd0e91b68965f1d988366b978ae1a84
Veanty <plmulticraftpl@gmail.com>
2022-10-14 16:06:17 +0200

    Feat: add new solution for calculation thye factorial of a number

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int number = 1;
+	int factorialOfNumber = 1;
+	printf("Podaj liczbe calkowita:");
+	scanf_s("%d", &number);
+	printf("Podales liczbe: %d", number);
+	printf("\nLiczenie silni liczby %d...", number);
+	for (size_t i = 1; i <= number; i++)
+	{
+		factorialOfNumber *= i;
+	}
+	printf("\nSilnia liczby %d to %d", number, factorialOfNumber);
+	printf("\nKoniec programu.");
+	return 0;
+}
