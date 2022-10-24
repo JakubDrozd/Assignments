28a1b0e1ef09fe2d9debb62cc6f58b01bb15122c
Veanty <plmulticraftpl@gmail.com>
2022-10-24 14:41:00 +0200

    Feat: add new assigment to the project

---
+++ lab04_drozd.c
@@ -0,0 +1,182 @@
+/*===========================================================================*/
+/*                               ZADANIE 28                                  */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x;
+//	int y;
+//	printf("Program sluzy do wyznaczenia czy punkt podany przez uzytkownika lezy na pierwszej cwiartce ukladu wspolrzednych\nAutor: Jakub Drozd\n");
+//	printf("Prosze podac wpolrzedna \"x\": ");
+//	if (scanf_s("%d", &x) != 1) {
+//		scanf_s("%d", &x);
+//	}
+//	printf("Prosze podac wpolrzedna \"y\": ");
+//	if (scanf_s("%d", &y) != 1) {
+//		scanf_s("%d", &y);
+//	}
+//	if (x <= 0 || y <= 0)
+//	{
+//		printf("Punkt o podanych wspolrzednych x=%d i y=%d NIE nalezy do pierwszej cwiartki ukladu wspolrzednych", x, y);
+//		printf("\nKoniec programu.");
+//		return 0;
+//	}
+//	else
+//	{
+//		printf("Punkt o podanych wspolrzednych x=%d i y=%d NALEZY do pierwszej cwiartki ukladu wspolrzednych", x, y);
+//		printf("\nKoniec programu.");
+//		return 0;
+//	}
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 29                                 */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x;
+//	int y;
+//	printf("Program sluzy do wyznaczenia na ktorej cwiartce ukladu wspolrzednych lezy punkt podany przez uzytkownika\nAutor: Jakub Drozd\n");
+//	printf("Prosze podac wpolrzedna \"x\": ");
+//	if (scanf_s("%d", &x) != 1) {
+//		printf("Bledne dane.");
+//		printf("\nKoniec programu.");
+//		return 0;
+//	}
+//	printf("Prosze podac wpolrzedna \"y\": ");
+//	if (scanf_s("%d", &y) != 1) {
+//		printf("Bledne dane.");
+//		printf("\nKoniec programu.");
+//		return 0;
+//	}
+//	if (x > 0)
+//	{
+//		if (y > 0)
+//		{
+//			printf("Punkt nalezy do I cwiartki ukladu wspolrzednych");
+//			printf("\nKoniec programu");
+//			return 0;
+//		}
+//		else if (y < 0)
+//		{
+//			printf("Punkt nalezy do IV cwiartki ukladu wspolrzednych");
+//			printf("\nKoniec programu");
+//			return 0;
+//		}
+//		else
+//		{
+//			printf("Punkt lezy na osi Y");
+//			printf("\nKoniec programu");
+//			return 0;
+//		}
+//	}
+//	else
+//	{
+//		if (y > 0)
+//		{
+//			printf("Punkt nalezy do II cwiartki ukladu wspolrzednych");
+//			printf("\nKoniec programu");
+//			return 0;
+//		}
+//		else if (y < 0)
+//		{
+//			printf("Punkt nalezy do III cwiartki ukladu wspolrzednych");
+//			printf("\nKoniec programu");
+//			return 0;
+//		}
+//		else
+//		{
+//			printf("Punkt lezy na osi Y");
+//			printf("\nKoniec programu");
+//			return 0;
+//		}
+//	}
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 30                                  */
+/*===========================================================================*/
+
+
+#include <stdio.h>
+
+int main()
+{
+
+}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 31                                  */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 32                                  */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 33                                  */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 34                                  */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 35                                  */
+/*===========================================================================*/
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//}
+
+
