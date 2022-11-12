d674fed25ba3db9021ca5a6a611786a5504f018e
Veanty <plmulticraftpl@gmail.com>
2022-11-12 15:46:14 +0100

    Refactor: comment out function for further tasks

--- lab04_drozd.c
+++ lab04_drozd.c
@@ -3,34 +3,34 @@
 /*===========================================================================*/
 
 
-#include <stdio.h>
-
-int main()
-{
-	int x;
-	int y;
-	printf("Program sluzy do wyznaczenia czy punkt podany przez uzytkownika lezy na pierwszej cwiartce ukladu wspolrzednych\nAutor: Jakub Drozd\n");
-	printf("Prosze podac wpolrzedna \"x\": ");
-	if (scanf_s("%d", &x) != 1) {
-		scanf_s("%d", &x);
-	}
-	printf("Prosze podac wpolrzedna \"y\": ");
-	if (scanf_s("%d", &y) != 1) {
-		scanf_s("%d", &y);
-	}
-	if (x <= 0 || y <= 0)
-	{
-		printf("Punkt o podanych wspolrzednych x=%d i y=%d NIE nalezy do pierwszej cwiartki ukladu wspolrzednych", x, y);
-		printf("\nKoniec programu.");
-		return 0;
-	}
-	else
-	{
-		printf("Punkt o podanych wspolrzednych x=%d i y=%d NALEZY do pierwszej cwiartki ukladu wspolrzednych", x, y);
-		printf("\nKoniec programu.");
-		return 0;
-	}
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x;
+//	int y;
+//	printf("Program sluzy do wyznaczenia czy punkt podany przez uzytkownika lezy na pierwszej cwiartce ukladu wspolrzednych\nAutor: Jakub Drozd\n");
+//	printf("Prosze podac wpolrzedna \"x\": ");
+//	if (scanf_s("%d", &x) != 1) {
+//		scanf_s("%d", &x);
+//	}
+//	printf("Prosze podac wpolrzedna \"y\": ");
+//	if (scanf_s("%d", &y) != 1) {
+//		scanf_s("%d", &y);
+//	}
+//	if (x <= 0 || y <= 0)
+//	{
+//		printf("Punkt o podanych wspolrzednych x=%d i y=%d NIE nalezy do pierwszej cwiartki ukladu wspolrzednych", x, y);
+//		printf("\nKoniec programu.");
+//		return 0;
+//	}
+//	else
+//	{
+//		printf("Punkt o podanych wspolrzednych x=%d i y=%d NALEZY do pierwszej cwiartki ukladu wspolrzednych", x, y);
+//		printf("\nKoniec programu.");
+//		return 0;
+//	}
+//}
 
 
 /*===========================================================================*/
