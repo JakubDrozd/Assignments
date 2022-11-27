1113cd98d4f8a9b20b154580035657d628c4a9e1
Veanty <plmulticraftpl@gmail.com>
2022-11-27 15:04:55 +0100

    Feat: complete task 56

--- lab07_drozd.c
+++ lab07_drozd.c
@@ -87,34 +87,34 @@
 /*===========================================================================*/
 
 
-#include <stdio.h>
-#include <limits.h>
-
-int main()
-{
-	printf("Program wyznacza minimum z podanych liczb\nAutor: Jakub Drozd\n");
-	printf("Podaj liczby naturalne, aby zakonczyc wpisz 0\n");
-	int liczba = 1;
-	int min = INT_MAX;
-	while (liczba != 0)
-	{
-		printf("Podaj liczbe: ");
-		while (scanf_s("%d", &liczba)!=1 || liczba < 0 || getchar()!='\n')
-		{
-			printf("Bledne dane, popraw: ");
-			int c;
-			while ((c = getchar()) != '\n' && c != EOF)
-				;
-		}
-		if (liczba != 0 && liczba < min)
-		{
-			min = liczba;
-		}
-	}
-	printf("Najmniejsza z podanych liczb to: %d", min);
-	printf("\nKoniec programu\n");
-	return 0;
-}
+//#include <stdio.h>
+//#include <limits.h>
+//
+//int main()
+//{
+//	printf("Program wyznacza minimum z podanych liczb\nAutor: Jakub Drozd\n");
+//	printf("Podaj liczby naturalne, aby zakonczyc wpisz 0\n");
+//	int liczba = 1;
+//	int min = INT_MAX;
+//	while (liczba != 0)
+//	{
+//		printf("Podaj liczbe: ");
+//		while (scanf_s("%d", &liczba)!=1 || liczba < 0 || getchar()!='\n')
+//		{
+//			printf("Bledne dane, popraw: ");
+//			int c;
+//			while ((c = getchar()) != '\n' && c != EOF)
+//				;
+//		}
+//		if (liczba != 0 && liczba < min)
+//		{
+//			min = liczba;
+//		}
+//	}
+//	printf("Najmniejsza z podanych liczb to: %d", min);
+//	printf("\nKoniec programu\n");
+//	return 0;
+//}
 
 
 /*===========================================================================*/
@@ -122,12 +122,33 @@ int main()
 /*===========================================================================*/
 
 
-//#include <stdio.h>
-//
-//int main()
-//{
-//
-//}
+#include <stdio.h>
+
+int main()
+{
+	int ilosc = 1;
+	double liczba;
+	double suma = 0;
+	printf("Program oblicza srednia dla podanych liczb zmiennopozycyjnych.\nAutor: Jakub Drozd\n");
+	printf("Podaj 10 liczb zmiennopozycyjnych:\n");
+	while (ilosc <= 10)
+	{
+		printf("%d. ", ilosc);
+		while (scanf_s("%lf", &liczba)!=1 || getchar()!='\n')
+		{
+			printf("Bledne dane, popraw: ");
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF)
+				;
+		}
+		suma += liczba;
+		ilosc++;
+	}
+	double srednia = suma / 10;
+	printf("Srednia arytmetyczna podanych liczb wynosi: %lf", srednia);
+	printf("\nKoniec programu\n");
+	return 0;
+}
 
 
 /*===========================================================================*/
