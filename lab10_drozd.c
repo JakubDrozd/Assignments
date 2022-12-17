df89c9ad5fe78160038300950dbfd374b750e0e7
Veanty <plmulticraftpl@gmail.com>
2022-12-17 13:09:58 +0100

    Feat: complete task 81

--- lab10_drozd.c
+++ lab10_drozd.c
@@ -3,73 +3,73 @@
 /*===========================================================================*/
 
 
-//#include <stdio.h>
-//
-//#define ILE 10 // ilosc pobranych liczb
-//
-//void info() {	//Wypisuje funkcje programu
-//	printf("Program wczytuje do tablicy liczby i wypisuje ja na dwa sposoby.\nAutor: Jakub Drozd\n");
-//}
-//
-//int wczytaj_int(void) {	//Wczytuje liczbe z bufora klawiatury
-//	int liczba;
-//	while (scanf_s("%d", &liczba)!=1 || getchar()!='\n')
-//	{
-//		printf("Bledne dane, popraw:\t\t");
-//		int c;
-//		while ((c = getchar()) != '\n' && c != EOF)
-//			;
-//	}
-//	return liczba;
-//}
-//
-//void wczytaj_int_1W(int tab[], int n) {	//Wczytuje liczbe z bufora klawiatury do tablicy
-//	for (int i = 0; i < n; i++)
-//	{
-//		printf("Liczba %d: ", i + 1);
-//		tab[i] = wczytaj_int();
-//	}
-//}
-//
-//void wypisz_liczby(int tab[], int n) {	//Wypisuje zawartosc tablicy w domyslnej i odwroconej kolejnosci
-//	printf("Podane liczby: [");
-//	for (int i = 0; i < n; i++)
-//	{
-//		if (i == n-1)
-//		{
-//			printf("%d", tab[i]);
-//		}
-//		else
-//		{
-//		printf("%d, ", tab[i]);
-//
-//		}
-//	}
-//	printf("]\n");
-//	printf("Podane liczby (odwrocone): [");
-//	for (int i = n-1; i >=0; --i)
-//	{
-//		if (i == 0)
-//		{
-//			printf("%d", tab[i]);
-//		}
-//		else
-//		{
-//			printf("%d, ", tab[i]);
-//		}
-//	}
-//	printf("]");
-//}
-//
-//int main()
-//{
-//	info();
-//	int liczby[ILE] = {0};
-//	wczytaj_int_1W(liczby, ILE);
-//	wypisz_liczby(liczby, ILE);
-//	printf("\n\nKoniec programu.\n\n");
-//	return 0;
-//}
+#include <stdio.h>
+
+#define ILE 10 // ilosc pobranych liczb
+
+void info() {	//Wypisuje funkcje programu
+	printf("Program wczytuje do tablicy liczby i wypisuje ja na dwa sposoby.\nAutor: Jakub Drozd\n");
+}
+
+int wczytaj_int(void) {	//Wczytuje liczbe z bufora klawiatury
+	int liczba;
+	while (scanf_s("%d", &liczba)!=1 || getchar()!='\n')
+	{
+		printf("Bledne dane, popraw:\t\t");
+		int c;
+		while ((c = getchar()) != '\n' && c != EOF)
+			;
+	}
+	return liczba;
+}
+
+void wczytaj_int_1W(int tab[], int n) {	//Wczytuje liczbe z bufora klawiatury do tablicy
+	for (int i = 0; i < n; i++)
+	{
+		printf("Liczba %d: ", i + 1);
+		tab[i] = wczytaj_int();
+	}
+}
+
+void wypisz_liczby(int tab[], int n) {	//Wypisuje zawartosc tablicy w domyslnej i odwroconej kolejnosci
+	printf("Podane liczby: [");
+	for (int i = 0; i < n; i++)
+	{
+		if (i == n-1)
+		{
+			printf("%d", tab[i]);
+		}
+		else
+		{
+		printf("%d, ", tab[i]);
+
+		}
+	}
+	printf("]\n");
+	printf("Podane liczby (odwrocone): [");
+	for (int i = n-1; i >=0; --i)
+	{
+		if (i == 0)
+		{
+			printf("%d", tab[i]);
+		}
+		else
+		{
+			printf("%d, ", tab[i]);
+		}
+	}
+	printf("]");
+}
+
+int main()
+{
+	info();
+	int liczby[ILE] = {0};
+	wczytaj_int_1W(liczby, ILE);
+	wypisz_liczby(liczby, ILE);
+	printf("\n\nKoniec programu.\n\n");
+	return 0;
+}
 
 
 /*===========================================================================*/
@@ -459,6 +459,8 @@
 //	wczytajLiczby(tab, 10);
 //	sortowanie(tab, 10);
 //	wypiszLiczby(tab, 10);
+//	printf("\n\nKoniec programu.\n\n");
+//	return 0;
 //}
 
 
