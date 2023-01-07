90e82560da14342c896c42ae6f3b86e49bd7c82b
Veanty <plmulticraftpl@gmail.com>
2023-01-07 13:53:25 +0100

    Feat: complete task 88

--- lab12_drozd.c
+++ lab12_drozd.c
@@ -3,44 +3,98 @@
 /*===========================================================================*/
 
 
+//#include <stdio.h>
+//#define MAX 25
+//
+//void info() {
+//	printf("Program wczytuje i wypisuje elementy tabeli dwuwymiarowej.\nAutor: Jakub Drozd\n");
+//}
+//
+//int wczytajLiczbe() {
+//	int liczba;
+//	while (scanf_s("%d", &liczba)!=1 || liczba < 0 || getchar()!='\n')
+//	{
+//		printf("Bledne dane, podaj liczbe calkowita wieksza od zera: ");
+//		int c;
+//		while ((c = getchar())!='\n' && c!=EOF)
+//			;
+//	}
+//	return liczba;
+//}
+//
+//double wczytajDouble() {
+//	double liczba;
+//	while (scanf_s("%lf", &liczba) != 1 || getchar() != '\n')
+//	{
+//		printf("Bledne dane, podaj liczbe zmiennopozycyjna: ");
+//		int c;
+//		while ((c = getchar()) != '\n' && c != EOF)
+//			;
+//	}
+//	return liczba;
+//}
+//
+//void wczytajDoTablicy(double tab[][MAX], int m, int n) {
+//	for (int i = 0; i < m; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			printf("Podaj nastepny element tablicy [%d][%d]: ", i+1, j+1);
+//			tab[i][j] = wczytajDouble();
+//		}
+//	}
+//}
+//
+//void wypiszTablice(double tab[][MAX], int m, int n) {
+//	for (int i = 0; i < m; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			printf("%.1lf ", tab[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//
+//int main()
+//{
+//	info();
+//	double tab[MAX][MAX];
+//	printf("Podaj liczbe wierszy tablicy: ");
+//	int wiersze = wczytajLiczbe();
+//	printf("Podaj liczbe kolumn tablicy: ");
+//	int kolumny = wczytajLiczbe();
+//	printf("Wiersze: %d\nKolumny: %d\n", wiersze, kolumny);
+//	wczytajDoTablicy(tab, wiersze, kolumny);
+//	wypiszTablice(tab, wiersze, kolumny);
+//	printf("\n\nKoniec programu.\n\n");
+//	return 0;
+//}
+
+
+/*===========================================================================*/
+/*                               ZADANIE 88                                  */
+/*===========================================================================*/
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define MAX 25
 
 void info() {
-	printf("Program wczytuje i wypisuje elementy tabeli dwuwymiarowej.\nAutor: Jakub Drozd\n");
+	printf("Program tworzy losowa tablice dwuwymiarowa i zamienia jej wiersze z kolumnami.\nAutor: Jakub Drozd\n");
 }
 
-int wczytajLiczbe() {
-	int liczba;
-	while (scanf_s("%d", &liczba)!=1 || liczba < 0 || getchar()!='\n')
-	{
-		printf("Bledne dane, podaj liczbe calkowita wieksza od zera: ");
-		int c;
-		while ((c = getchar())!='\n' && c!=EOF)
-			;
-	}
-	return liczba;
-}
-
-double wczytajDouble() {
-	double liczba;
-	while (scanf_s("%lf", &liczba) != 1 || getchar() != '\n')
-	{
-		printf("Bledne dane, podaj liczbe zmiennopozycyjna: ");
-		int c;
-		while ((c = getchar()) != '\n' && c != EOF)
-			;
-	}
-	return liczba;
-}
 
 void wczytajDoTablicy(double tab[][MAX], int m, int n) {
+	srand((unsigned)time(NULL));
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			printf("Podaj nastepny element tablicy [%d][%d]: ", i+1, j+1);
-			tab[i][j] = wczytajDouble();
+			tab[i][j] = rand() % (8 + 1 + 8) - 8;
 		}
 	}
 }
@@ -50,7 +104,18 @@ void wypiszTablice(double tab[][MAX], int m, int n) {
 	{
 		for (int j = 0; j < n; j++)
 		{
-			printf("%.1lf ", tab[i][j]);
+			printf("%4.1lf ", tab[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void transponowanie(double tab[][MAX], int m, int n) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			printf("%4.1lf ", tab[j][i]);
 		}
 		printf("\n");
 	}
@@ -61,29 +126,15 @@ int main()
 {
 	info();
 	double tab[MAX][MAX];
-	printf("Podaj liczbe wierszy tablicy: ");
-	int wiersze = wczytajLiczbe();
-	printf("Podaj liczbe kolumn tablicy: ");
-	int kolumny = wczytajLiczbe();
-	printf("Wiersze: %d\nKolumny: %d\n", wiersze, kolumny);
-	wczytajDoTablicy(tab, wiersze, kolumny);
-	wypiszTablice(tab, wiersze, kolumny);
+	printf("Wiersze: %d\nKolumny: %d\n", 3, 2);
+	wczytajDoTablicy(tab, 3, 2);
+	printf("Tablica:\n");
+	wypiszTablice(tab, 3, 2);
+	printf("Tablica (po transponowaniu):\n");
+	transponowanie(tab, 3, 2);
 	printf("\n\nKoniec programu.\n\n");
 	return 0;
 }
-
-
-/*===========================================================================*/
-/*                               ZADANIE 88                                  */
-/*===========================================================================*/
-
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//
-//}
 
 
 /*===========================================================================*/
