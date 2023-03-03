1a144cce4e68f72954b35ab247d17898b20dd7e6
Veanty <plmulticraftpl@gmail.com>
2023-03-03 17:03:18 +0100

    Feat: complete enter task lab03

--- wejsciowka03_jakub_drozd.c
+++ wejsciowka03_jakub_drozd.c
@@ -30,19 +30,42 @@ int funkcja(struct nowy_typ_t tab[], int m) {
 	return liczbaSkrocen;
 }
 
-void wypisz_tab_1W(struct nowy_typ_t tab[], int m, int n) {
+void wypisz_tab_1W(struct nowy_typ_t tab[], int m) {
 	for (int i = 0; i < m; i++)
 	{
 		printf("%s", tab[i].napis);
-		printf("%2d", tab[i].liczba);
+		printf("%2d\n", tab[i].liczba);
 	}
 }
 
 
+int wczytajLiczbe() {
+	int liczba;
+	while (scanf_s("%d", &liczba)!=1 || getchar()!='\n')
+	{
+		printf("Bledne dane, podaj liczbe: ");
+		int c;
+		while ((c = getchar()) != '\n' && c != EOF)
+			;
+	}
+	return liczba;
+}
+
+
+
+void wczytaj_dane_tab_1W(struct nowy_typ_t tab[], int m) {
+	for (int i = 0; i < m; i++)
+	{
+		printf("Podaj napis dla [%d]:%3s", i+1, "");
+		scanf_s("%s", tab[i].napis, MAX_ZNAKOW + 1);
+		printf("Podaj liczbe dla [%d]:%2s", i+1, "");
+		tab[i].liczba = wczytajLiczbe();
+	}
+}
+
 int main() {
 	struct nowy_typ_t tab[MAX_ROZMIAR] = {NULL};
-	tab[0].liczba = 3;
-	strcpy_s(tab[0].napis, strlen("jakub")+1, "jakub");
+	wczytaj_dane_tab_1W(tab, MAX_ROZMIAR);
 	int liczbaSkrocen = funkcja(tab, MAX_ROZMIAR);
 	wypisz_tab_1W(tab, MAX_ROZMIAR, MAX_ZNAKOW);
 	printf("\n\nLiczba skrocen: %d\n\n", liczbaSkrocen);
