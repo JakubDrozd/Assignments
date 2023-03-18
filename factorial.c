158e5a81ba1d0f9775e7d3776fa7aff7642e9c01
Veanty <plmulticraftpl@gmail.com>
2023-03-18 14:31:34 +0100

    Feat: app now reads input text data and creates a one way list

--- factorial.c
+++ factorial.c
@@ -1437,3 +1437,96 @@ int main(void) {
 //    }
 //    return 0;
 //}
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct student {
+    int indeks;
+    char imie[25];
+    int ocena;
+    struct student* nastepny;
+};
+
+struct student* dodajStudenta(int indeks, char imie[], int ocena) {
+    struct student* nowyStudent = NULL;
+    nowyStudent = malloc(sizeof(struct student));
+    if (nowyStudent != NULL)
+    {
+        nowyStudent->indeks = indeks;
+        strcpy_s(nowyStudent->imie, sizeof(imie), imie);
+        nowyStudent->ocena = ocena;
+        nowyStudent->nastepny = NULL;
+        printf("\nDodano studenta pod adresem %p\n", nowyStudent);
+    }
+    else
+    {
+        printf("Blad alokowania pamieci");
+    }
+    return nowyStudent;
+}
+
+ void wypiszStudenta(struct student *student) {
+     if (student == NULL)
+     {
+         printf("Student nie istnieje");
+     }
+     else
+     {
+         printf("\n[Indeks]: %d\n[Imie]: %s\n[Ocena]: %d\n[Nastepny]: %p\n",
+             student->indeks,
+             student->imie,
+             student->ocena,
+             student->nastepny
+             );
+     }
+ }
+
+ void wypiszStudentow(struct student* pierwszy) {
+     struct student* aktualny = pierwszy;
+         while (aktualny != NULL)
+         {
+             wypiszStudenta(aktualny);
+             aktualny = aktualny->nastepny;
+         }
+}
+
+int main(void) {
+    FILE* input;
+    struct student* pierwszy = NULL;    //Wskazuje na pierwszy element z listy
+    struct student* aktualny = NULL;    //Wskazuje na ostatnio dodany element z listy
+    if (fopen_s(&input, "C:\\Users\\PATOX\\Desktop\\dane.txt", "r")!=0)
+    {
+        printf("Blad wczytywania pliku");
+        return 1;
+    }
+    int indeks = 0;
+    char imie[25];
+    int ocena = 0;
+    while (fscanf_s(input,"%d %s %d", &indeks, imie, (unsigned)sizeof(imie), &ocena)==3)  //Dopóki jest w w stanie odczytać dane w formacie, pętla się wykonuje
+    {
+        if (pierwszy == NULL)                                                             
+        {
+            pierwszy = dodajStudenta(indeks, imie, ocena);                                //Jeżeli lista nie posiada jeszcze pierwszego elementu, tworzy go
+            if (pierwszy!=NULL)                                                           
+            {
+                aktualny = pierwszy;                                                        //Jeżeli alokacja w pamięci się udała, ustaw dodaną daną jako dodaną
+            }
+        }
+        else
+        {
+            aktualny->nastepny = dodajStudenta(indeks, imie, ocena);                        //Jeżeli lista zawiera już pierwszy element, dodaje kolejny
+            if (aktualny->nastepny!=NULL)
+            {
+                aktualny = aktualny->nastepny;                                              //Jeżeli alokacja w pamieci sie udala, to ustawiamy wskaznik aktualny                                                                            na nastepny element, ktory kiedys dodamy
+            }
+        }
+    }
+    wypiszStudentow(pierwszy);
+    free(pierwszy);
+    free(aktualny);
+    pierwszy = NULL;
+    aktualny = NULL;
+    return 0;
+}
