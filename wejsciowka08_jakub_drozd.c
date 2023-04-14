76ee0c107f311db10133437537e4d0e8453c4ac8
Veanty <plmulticraftpl@gmail.com>
2023-04-14 15:48:33 +0200

    Feat: finish entry task lab 08

--- wejsciowka08_jakub_drozd.c
+++ wejsciowka08_jakub_drozd.c
@@ -7,6 +7,38 @@
 #include <string.h>
 #include <Windows.h>
 
+void loguj(char* akcja) {
+	FILE* log;
+	time_t czas;
+	char czas_str[30];
+	char err_msg[100];
+	time(&czas);
+	struct tm local_time;
+	if (localtime_s(&local_time, &czas) != 0) {
+		strerror_s(err_msg, sizeof(err_msg), errno);
+		fprintf(stderr, "Blad funkcji localtime_s: %s\n", err_msg);
+		return;
+	}
+	if (strftime(czas_str, sizeof(czas_str), "%Y-%m-%d %H:%M:%S", &local_time) == 0) {
+		fprintf(stderr, "Blad funkcji strftime\n");
+		return;
+	}
+	errno_t err;
+	if ((err = fopen_s(&log, "log.txt", "a")) != 0) {
+		strerror_s(err_msg, sizeof(err_msg), err);
+		fprintf(stderr, "Blad otwarcia pliku: %s\n", err_msg);
+		return;
+	}
+	if (log != NULL) {
+		fprintf(log, "[%s] | %s\n", czas_str, akcja);
+		fclose(log);
+	}
+	else {
+		strerror_s(err_msg, sizeof(err_msg), errno);
+		fprintf(stderr, "Blad funkcji fopen_s: %s\n", err_msg);
+	}
+}
+
 char podaj_nick(char nick[]) {
 	nick[24] = '\0';
 	scanf_s("%s", nick, 25);
@@ -36,10 +68,16 @@ void test_start(int liczba, int* punkty, double *czas_testu) {
 	{
 		char litera = rand() % (122 + 1 - 97) + 97;
 		printf("%c\n", litera);
+		loguj("Wypisanie litery na ekran");
 		odpowiedz = _getch();
 		if (odpowiedz == litera)
 		{
 			(*punkty)++;
+			loguj("Podanie poprawnej odpowiedzi");
+		}
+		else
+		{
+			loguj("Podanie blednej odpowiedzi");
 		}
 	}
 	time(&koniec_testu);
@@ -55,8 +93,10 @@ int main() {
 	char nick[25];
 	podaj_nick(nick);
 	printf("Nick: %s\n", nick);
+	loguj("Pobranie nick'a");
 	int liczba = podaj_liczbe();
 	printf("Liczba: %d\n", liczba);
+	loguj("Pobranie liczby");
 	int punkty = 0;
 	test_start(liczba, &punkty, &czas_testu);
 	char sciezka[30];
