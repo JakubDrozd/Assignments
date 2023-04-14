cb315011211c0d85f6518ca0cd783cd3652cd9b8
Veanty <plmulticraftpl@gmail.com>
2023-04-14 14:47:52 +0200

    Feat: now creates a file with scoreboard and reads file path

--- wejsciowka08_jakub_drozd.c
+++ wejsciowka08_jakub_drozd.c
@@ -4,6 +4,8 @@
 #include <assert.h>
 #include <time.h>
 #include <conio.h>
+#include <string.h>
+#include <Windows.h>
 
 char podaj_nick(char nick[]) {
 	nick[24] = '\0';
@@ -25,40 +27,64 @@ void wypisz_tab(double tab[], int m) {
 	}
 }
 
-void test_start(int liczba, double wyniki[], int* punkty) {
-	time_t start;
-	time_t koniec;
-	double czas;
+void test_start(int liczba, int* punkty) {
+	time_t start_testu;
+	time_t koniec_testu;
+	double czas_testu;
 	char odpowiedz;
-
+	time(&start_testu);
 	for (int i = 0; i < liczba ; i++)
 	{
 		char litera = rand() % (122 + 1 - 97) + 97;
 		printf("%c\n", litera);
-		time(&start);
 		odpowiedz = _getch();
-		time(&koniec);
 		if (odpowiedz == litera)
 		{
 			(*punkty)++;
 		}
-		czas = difftime(koniec, start);
-		wyniki[i] = czas;
 	}
+	time(&koniec_testu);
+	czas_testu = difftime(koniec_testu, start_testu);
+	printf("Czas testu : %.1lf%c\n", czas_testu, 's');
+	printf("Wynik: %d\n", *punkty);
 }
 
 int main() {
 	srand((unsigned)time(NULL));
+	FILE* wyjscie;
 	char nick[25];
 	podaj_nick(nick);
 	printf("Nick: %s\n", nick);
 	int liczba = podaj_liczbe();
 	printf("Liczba: %d\n", liczba);
-	double* wyniki = malloc(liczba * sizeof(double));
 	int punkty = 0;
-	test_start(liczba, wyniki, &punkty);
-	printf("\n\n");
-	wypisz_tab(wyniki, liczba);
-	printf("\nWynik: %d", punkty);
+	test_start(liczba, &punkty);
+	char sciezka[30];
+	sciezka[29] = '\0';
+	strcpy_s(sciezka, sizeof(sciezka), nick);
+	strcat_s(sciezka, sizeof(sciezka), ".txt");
+	if (fopen_s(&wyjscie, sciezka, "r") != 0)
+	{
+		printf("!!!Plik nie istnieje, tworzenie nowego pliku!!!\n");
+		wchar_t cwd[MAX_PATH];
+		if (GetCurrentDirectory(MAX_PATH, cwd) != 0) {
+			wprintf(L"Sciezka: %ls\n", cwd);
+		}
+		printf("\nNazwa pliku: %s\n", sciezka);
+		fopen_s(&wyjscie, sciezka, "w");
+	}
+	else
+	{
+		printf("\nPlik juz instnieje\n");
+		fclose(wyjscie);
+		fopen_s(&wyjscie, sciezka, "a");
+	}
+	if (wyjscie == NULL)
+	{
+		printf("\nBlad otwarcia pliku\n");
+		exit(0);
+	}
+	fprintf_s(wyjscie, "%s", "sukces\n");
+	fclose(wyjscie);
 	return 0;
 }
